6d1a74fda1bfdba99ff0b75d8099e25b3498c08c
kp0hyc <kp0hyc@mail.ru>
2017-11-15 14:22:18 +0300

    add some diff
    added comment that it is git

--- Assets/Plane/scripts/NetworkScript.cs
+++ Assets/Plane/scripts/NetworkScript.cs
@@ -6,6 +6,7 @@ using UnityEngine.Networking;
 
 public class NetworkScript : NetworkBehaviour 
 {
+	//git test change
 	/*class SyncListNickname : SyncList<string>
 	{
 		protected override void SerializeItem (NetworkWriter writer, string item)
